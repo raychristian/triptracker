6d446a5dd4d48de7f4c249e551639029cfcfbd84
raychristian <raymond.a.christian@gmail.com>
2023-08-17 17:29:12 -0500

    v2 video sync to database attempt home & views.py

--- app/triptracker/views.py
+++ app/triptracker/views.py
@@ -11,6 +11,33 @@ from .services import create_new_user
 from django.templatetags.static import static
 from django.urls import reverse
 
+from google.cloud import storage
+from django.http import JsonResponse
+
+from triptracker.models import UserGeneratedVideo
+from google.cloud import ndb
+import datetime
+import uuid
+
+
+def get_signed_url(request):
+    if request.method == 'POST':
+        # Authentication and authorization checks...
+
+        # Generate a signed URL for uploading to GCS
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket('triptracker-394521.appspot.com')
+        blob = bucket.blob('videos/' + str(request.user.id) + '/video_filename.webm')
+        
+        # Generate a signed URL for this blob that lasts for 300 seconds (5 minutes)
+        signed_url = blob.generate_signed_url(expiration=300, method='PUT')
+        
+        # Return the signed URL to the client
+        return JsonResponse({'signed_url': signed_url})
+
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=400)
+
 
 def dashboard(request):
     videos = [
@@ -77,3 +104,28 @@ def myaccount_request(request):
 
 def faq_view(request):
     return render(request, 'triptracker/faq.html')
+
+def save_video_metadata(user_id, video_url):
+    """
+    Save metadata about a user's video recording in Google Cloud Datastore.
+    
+    Parameters:
+        user_id (str): The ID of the user who created the video.
+        video_url (str): The URL of the video in Google Cloud Storage.
+        Add additional fields (from models.py) later
+    """
+    
+    with ndb.Client().context():
+        # Create a new UserGeneratedVideo entity
+        user_video = UserGeneratedVideo(
+            userID=ndb.Key('UserProfile', user_id),
+            videoID = ndb.StringProperty(default=lambda: str(uuid.uuid4())),
+            videoURL=video_url,
+            timestamp=datetime.datetime.now()  # Automatically set the current date and time
+        )
+        # Save the entity to Datastore
+        user_video.put()
+    
+    print('Saved video metadata for user:', user_id)
+
+
